749fdbbdc7366d05c1f29064becad3d22c30b2b4
John Bae <john21225@gmail.com>
2022-01-08 16:47:18 -0800

    Simple world map showing

--- app/src/components/CircleMark.js
+++ app/src/components/CircleMark.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { curveNatural, line } from "d3";
+import { geoEqualEarth, geoPath } from "d3";
+
+const projection = geoEqualEarth();
+const path = geoPath(projection);
 
 const CircleMark = ({ data }) => {
 	return (
 		<g className="marks">
-			{data.map((d) => (
-				<path d={d} />
+			{data.features.map((feature) => (
+				<path d={path(feature)} />
 			))}
 		</g>
 	);
