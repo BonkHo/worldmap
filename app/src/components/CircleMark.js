c7ee6e86e1249f93d4a05bea4035bd12ba382171
John Bae <john21225@gmail.com>
2022-01-08 16:43:40 -0800

    Formatted app

--- app/src/components/CircleMark.js
+++ app/src/components/CircleMark.js
@@ -1,29 +1,11 @@
 import React from "react";
 import { curveNatural, line } from "d3";
 
-const CircleMark = ({
-	data,
-	yScale,
-	xScale,
-	yValue,
-	xValue,
-	toolTipFormat,
-	markRadius = 3,
-}) => {
+const CircleMark = ({ data }) => {
 	return (
 		<g className="marks">
-			<path
-				stroke="black"
-				fill="none"
-				d={line()
-					.x((d) => xScale(xValue(d)))
-					.y((d) => yScale(yValue(d)))
-					.curve(curveNatural)(data)}
-			/>
 			{data.map((d) => (
-				<circle cx={xScale(xValue(d))} cy={yScale(yValue(d))} r={markRadius}>
-					<title>{toolTipFormat(xValue(d))}</title>
-				</circle>
+				<path d={d} />
 			))}
 		</g>
 	);
