ccbf4df68a2c473efcc5440a2ecf88efd832e7d1
John Bae <john21225@gmail.com>
2022-01-10 16:27:52 -0800

    Added graticules and finished styling

--- app/src/components/CircleMark.js
+++ app/src/components/CircleMark.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { geoEqualEarth, geoPath } from "d3";
+import { geoNaturalEarth1, geoPath, geoGraticule } from "d3";
 
-const projection = geoEqualEarth();
+const projection = geoNaturalEarth1();
 const path = geoPath(projection);
+const graticule = geoGraticule();
 
 const CircleMark = ({ data: { land, interiors } }) => {
 	return (
 		<g className="marks">
 			<path className="sphere" d={path({ type: "Sphere" })}></path>
+			<path className="graticule" d={path(graticule())}></path>
 			{land.features.map((feature) => (
 				<path className="land" d={path(feature)} />
 			))}
